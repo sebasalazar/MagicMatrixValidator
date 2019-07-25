a5a3f5c241128f9ab04b7a85cccf9f01d527cb18
Sebastián Salazar Molina <sebasalazar@gmail.com>
2019-07-24 20:54:12 -0400

    Arreglo en eliminación de string

--- app/src/main/java/cl/sebastian/matrix/validator/model/Matrix.java
+++ app/src/main/java/cl/sebastian/matrix/validator/model/Matrix.java
@@ -29,7 +29,8 @@ public class Matrix {
     public Matrix(String line) {
         this.data = new int[3][3];
         if (StringUtils.isNotBlank(line)) {
-            String str = StringUtils.trimToEmpty(StringUtils.remove(line, "[]"));
+            String uno = StringUtils.trimToEmpty(StringUtils.remove(line, "["));
+            String str = StringUtils.trimToEmpty(StringUtils.remove(uno, "]"));
             String[] arreglo = StringUtils.split(str, ";");
             for (int i = 0; i < arreglo.length; i++) {
                 String fila = StringUtils.trimToEmpty(arreglo[i]);
@@ -111,9 +112,9 @@ public class Matrix {
     }
 
     public String getString() {
-        return String.format("[%d,%d,%d;%d,%d,%d;%d,%d,%d]", 
-                data[0][0], data[0][1], data[0][2], 
-                data[1][0], data[1][1], data[1][2], 
+        return String.format("[%d,%d,%d;%d,%d,%d;%d,%d,%d]",
+                data[0][0], data[0][1], data[0][2],
+                data[1][0], data[1][1], data[1][2],
                 data[2][0], data[2][1], data[2][2]);
     }
 }
